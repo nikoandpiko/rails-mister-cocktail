07ae9d6ba7c06cced0cd664dc72ab75ddd6d9e7d
Nick Roma <nikoandpiko@gmail.com>
2021-01-18 15:07:46 +0900

    Creates functioning site

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+if Rails.env.development?
+  puts "Emptying glasses..."
+  Ingredient.destroy_all
+  Cocktail.destroy_all
+end
+
+puts "Adding drinks..."
+
+Cocktail.create(name: "Red-Headed Susan")
+Cocktail.create(name: "The PoPo")
+Cocktail.create(name: "El Magic Pepe")
+
+result = JSON.parse(open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+drinks = result['drinks']
+
+puts "Adding ingredients..."
+puts "Adding: "
+
+drinks.each do |hash|
+  hash.each do |key, val|
+    Ingredient.create(name: val)
+    print "♪┏(・o・)┛♪ #{val}, "
+  end
+end
+
+puts
+puts "Drinks are ready!"
