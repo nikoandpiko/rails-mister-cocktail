5029bf39148b613023723ee4925ca7ef2e321eca
Nick Roma <nikoandpiko@gmail.com>
2021-01-21 23:39:01 +0900

    Makes nicer, yet incomplete, buttons

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,8 +2,8 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["乾杯！", "Cheers!", "I created this myself!", "Just kidding."],
-    typeSpeed: 70,
+    strings: ["乾杯！", "Cheers!", "Santé!", "Salute!", "건배!", "Skål!", "Будем здоровы!", "ΥΓΕΙΑ!", "Proost!", "干杯!", "Na zdravi!"],
+    typeSpeed: 80,
     loop: true
   });
 }
